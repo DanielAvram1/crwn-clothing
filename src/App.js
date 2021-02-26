16921e26c2416c073eec5d6b6cadb198488b8a73
DanielAvram1 <daniel.avram@my.fmi.unibuc.ro>
2021-02-26 20:35:02 +0200

    broken our homepage into smallor components, finished styling our menu-component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
-import './homepage.styles.scss'
 
 function App() {
   return (
